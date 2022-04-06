8b338b83c5d239501631a08ad546a9c3c53d5860
Bondok6 <kirolousbondok6@gmail.com>
2022-04-06 17:59:57 +0200

    Add decorator.rb file

---
+++ decorator.rb
@@ -0,0 +1,32 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+class BaseDecorator < Nameable
+
+  attr_accessor :name
+
+  def initialize(name)
+    super()
+    @name = name
+  end
+
+  def correct_name
+    @name.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @name.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name 
+    return @name.correct_name[0..9] if(@name.correct_name.length > 10)
+    @name.correct_name
+  end
+end
