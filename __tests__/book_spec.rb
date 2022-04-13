34fb3bbb1348ea4d6947f5e8c4a237ae7e042bf4
Bondok6 <kirolousbondok6@gmail.com>
2022-04-13 21:34:28 +0200

    Create __tests Folder || Add test for Book class

---
+++ __tests__/book_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper'
+
+describe Book do 
+  context "when create a new book" do 
+     it "return a new object" do
+      book = Book.new('Learn Ruby', 'Dr.John')
+      expect(book).to be_an_instance_of(Book)
+     end 
+     it "return the correct title and author" do
+      book = Book.new('Learn Ruby', 'Dr.John')
+      expect(book.title).to eql('Learn Ruby')
+      expect(book.author).to eql('Dr.John')
+    end
+  end 
+end
---
+++ __tests__/spec_helper.rb
@@ -0,0 +1,6 @@
+require_relative '../book'
+require_relative '../person'
+require_relative '../student'
+require_relative '../teacher'
+require_relative '../classroom'
+require_relative '../rental'
